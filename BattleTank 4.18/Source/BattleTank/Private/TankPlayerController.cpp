b219630193716f569c7a932f4558e6333c1d3db3
Raiden <diablo_doomzy@yahoo.com>
2017-10-28 11:30:59 -0700

    Starting to create C++ classes

---
+++ BattleTank 4.18/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,22 @@
+// CopyRight@EagerBeaver
+
+#include "TankPlayerController.h"
+
+void ATankPlayerController::BeginPlay() {
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+
+	if (!ControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("Player Controller not possessing a tank!"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("Player Controller possessing %s"), *ControlledTank->GetName());
+	}
+}
+
+ATank* ATankPlayerController::GetControlledTank() const {
+	return Cast<ATank>(GetPawn());
+}
+
+
---
+++ BattleTank 4.18/Source/BattleTank/Public/TankPlayerController.h
@@ -0,0 +1,22 @@
+// CopyRight@EagerBeaver
+
+#pragma once
+
+#include "Tank.h"
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public: 
+	ATank* GetControlledTank() const;
+	
+	virtual void BeginPlay() override;
+};
