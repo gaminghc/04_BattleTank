01cb4c915b11dae5113305f3544e921d49f533ea
Raiden <diablo_doomzy@yahoo.com>
2017-10-28 12:52:31 -0700

    Tank AI

---
+++ BattleTank 4.18/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,39 @@
+// CopyRight@EagerBeaver
+
+#include "TankAIController.h"
+
+void ATankAIController::BeginPlay() {
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+
+	if (!ControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("AI Controller not possessing a tank!"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("AI Controller possessing %s"), *ControlledTank->GetName());
+	}
+
+	auto PlayerTank = GetPlayerTank();
+
+	if (!PlayerTank) {
+		UE_LOG(LogTemp, Warning, TEXT("AI Controller could not find player tank!"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("AI Controller found player tank %s"), *PlayerTank->GetName());
+	}
+}
+
+ATank* ATankAIController::GetControlledTank() const {
+	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const {
+	auto PlayerTank = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerTank) {
+		return nullptr;
+	} 
+	return (ATank*)(PlayerTank);
+}
+
+
---
+++ BattleTank 4.18/Source/BattleTank/Public/TankAIController.h
@@ -0,0 +1,27 @@
+// CopyRight@EagerBeaver
+
+#pragma once
+
+#include "Engine/World.h"
+#include "Tank.h"
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "TankAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+private:
+	ATank* GetControlledTank() const;
+
+	ATank* GetPlayerTank() const;
+
+	virtual void BeginPlay() override;
+	
+	
+};
