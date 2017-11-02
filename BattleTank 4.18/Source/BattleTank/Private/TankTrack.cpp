eb3daf86b7593a5521725621c7277c52430fd781
Raiden <diablo_doomzy@yahoo.com>
2017-11-02 00:30:43 -0700

    Being able to move

--- BattleTank 4.18/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank 4.18/Source/BattleTank/Private/TankTrack.cpp
@@ -4,6 +4,11 @@
 
 void UTankTrack::SetThrottle(float Throttle)
 {
+	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	UE_LOG(LogTemp, Warning, TEXT("%s shit tank track throttling at %f"), *GetName(), Throttle);
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
 
 
--- BattleTank 4.18/Source/BattleTank/Public/TankTrack.h
+++ BattleTank 4.18/Source/BattleTank/Public/TankTrack.h
@@ -10,7 +10,7 @@
 /**
  * Tank track is used to set maximum driving force, and to apply forces to the tank
  */
-UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent), hidecategories = ("Collision"))
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 {
 	GENERATED_BODY()
@@ -19,5 +19,7 @@ public:
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void SetThrottle(float Throttle);
 	
-	
+	// Max force per track, in Newtons
+	UPROPERTY(EditDefaultsOnly, Category = Setup)
+	float TrackMaxDrivingForce = 400000.f; // 40 tons tank, 1g accleration
 };
